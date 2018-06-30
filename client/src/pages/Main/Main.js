f805e2113b33c73c01d6cb2253bbbcff754a5b8d
Alexander <agonz519@gmail.com>
2018-06-30 06:56:28 -0400

    Finished logic for rendering articles and saving them

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -32,6 +32,36 @@ class Main extends Component {
       .catch(err => console.log(err));
   };
 
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.topic && this.state.startYear && this.state.endYear) {
+      API.runQuery(this.state.topic, this.state.startYear, this.state.endYear)
+        .then(res => {
+          console.log(res.data.response.docs);
+          this.setState({ articles: res.data.response.docs })
+        });
+      this.setState({ topic: "", startYear: "", endYear: "" });
+    }
+  };
+
+  handleSaveBtn = (title, url, date) => {
+    // event.preventDefault();
+    let article = {
+      title: title,
+      url: url,
+      date: date
+    };
+    API.saveArticle(article)
+      .then(res => this.loadArticles())
+  };
+
   render() {
     return (
       <Container fluid>
@@ -39,58 +69,61 @@ class Main extends Component {
           <Col size="md-12">
 
             <Jumbotron>
-              <h1>Read Some Articles Bruv</h1>
+              <h1>New York Times Article Search</h1>
             </Jumbotron>
 
             <h2>Search for Articles</h2>
             <form>
               <Input
-                // value={this.state.topic}
+                value={this.state.topic}
+                onChange={this.handleInputChange}
                 name="topic"
                 placeholder="Topic (required)"
               />
               <Input
-                // value={this.state.startYear}
+                value={this.state.startYear}
+                onChange={this.handleInputChange}
                 name="startYear"
                 placeholder="Start Year (required)"
               />
               <Input
-                // value={this.state.endYear}
+                value={this.state.endYear}
+                onChange={this.handleInputChange}
                 name="endYear"
                 placeholder="End Year (required)"
               />
               <FormBtn
                 disabled={!(this.state.topic && this.state.startYear && this.state.endYear)}
+                onClick={this.handleFormSubmit}
               >
                 Search
               </FormBtn>
             </form>
 
-            <br/><br/>
+            <br /><br />
             <hr />
 
             <h2>Results</h2>
-            <List>
-              <ListItem>
-                <strong>
-                Trump chokes out Teen after Teen Slaps Him
-                </strong>
-                <SaveBtn/>
-              </ListItem>
-              <ListItem>
-                <strong>
-                Man Wimps out on Kiss Cam
-                </strong>
-                <SaveBtn/>
-              </ListItem>
-              <ListItem>
-                <strong>
-                The Pope says "That's Dope"
-                </strong>
-                <SaveBtn/>
-              </ListItem>
-            </List>
+            {this.state.articles.length ? (
+              <List>
+                {this.state.articles.map((article, index) => (
+                  <ListItem key={index}>
+                    <strong>
+                      {article.headline.main}
+                    </strong>
+                    <br />
+                    Publication Date: {article.pub_date}
+                    <br />
+                    <a href={article.web_url} target="_blank">Click Here to Read the Article</a>
+                    <SaveBtn onClick={() => this.handleSaveBtn(article.headline.main, article.web_url, article.pub_date)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h4>There are no results to display. Enter a new search to see some results.</h4>
+            )}
 
+            <br/><br/>
             <hr />
 
             <h2>Saved Articles</h2>
@@ -101,6 +134,10 @@ class Main extends Component {
                     <strong>
                       {article.title}
                     </strong>
+                    <br />
+                    Publication Date: {article.date}
+                    <br />
+                    <a href={article.url} target="_blank">Click Here to Read the Article</a>
                     <DeleteBtn onClick={() => this.deleteArticle(article._id)} />
                   </ListItem>
                 ))}
